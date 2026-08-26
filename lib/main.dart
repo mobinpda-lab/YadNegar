3051a4400b30eb91ff9abd38af52198ad61d2fd2
mobinpda-lab <mobinpda@gmail.com>
2026-08-27 02:19:30 +0330

    feat(timeline): wire restore use case for delete undo

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:yadnegar/features/timeline/application/edit_timeline_item.dart';
 import 'package:yadnegar/features/timeline/application/filter_timeline_by_date_range.dart';
 import 'package:yadnegar/features/timeline/application/load_timeline.dart';
 import 'package:yadnegar/features/timeline/application/quick_capture.dart';
+import 'package:yadnegar/features/timeline/application/restore_timeline_item.dart';
 import 'package:yadnegar/features/timeline/application/search_timeline.dart';
 import 'package:yadnegar/features/timeline/data/json_file_timeline_repository.dart';
 import 'package:yadnegar/features/timeline/presentation/timeline_home.dart';
@@ -39,6 +40,7 @@ Future<void> main() async {
         loadTimeline: LoadTimeline(repository: repository),
         editTimelineItem: EditTimelineItem(repository: repository),
         deleteTimelineItem: DeleteTimelineItem(repository: repository),
+        restoreTimelineItem: RestoreTimelineItem(repository: repository),
         searchTimeline: SearchTimeline(repository: repository),
         filterTimelineByDateRange: FilterTimelineByDateRange(
           repository: repository,
