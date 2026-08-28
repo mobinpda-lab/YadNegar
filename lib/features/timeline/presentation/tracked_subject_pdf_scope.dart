1c19cce6973f877c84b119d9a7c935a2433bcb11
mobinpda-lab <mobinpda@gmail.com>
2026-08-28 16:35:34 +0330

    feat: expose tracked tasks to PDF scope selector

--- lib/features/timeline/presentation/tracked_subject_pdf_scope.dart
+++ lib/features/timeline/presentation/tracked_subject_pdf_scope.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/widgets.dart';
+import 'package:yadnegar/features/timeline/domain/timeline_item.dart';
 
 typedef TrackedSubjectPdfAction = Future<void> Function(Set<String>? subjectIds);
+typedef TrackedSubjectPdfSubjectsLoader = Future<List<TimelineItem>> Function();
 
 class TrackedSubjectPdfScope extends InheritedWidget {
   const TrackedSubjectPdfScope({
     super.key,
     required this.sharePdf,
     required this.printPdf,
+    required this.loadSubjects,
     required super.child,
   });
 
   final TrackedSubjectPdfAction sharePdf;
   final TrackedSubjectPdfAction printPdf;
+  final TrackedSubjectPdfSubjectsLoader loadSubjects;
 
   static TrackedSubjectPdfScope? maybeOf(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<TrackedSubjectPdfScope>();
@@ -19,6 +23,8 @@ class TrackedSubjectPdfScope extends InheritedWidget {
 
   @override
   bool updateShouldNotify(TrackedSubjectPdfScope oldWidget) {
-    return sharePdf != oldWidget.sharePdf || printPdf != oldWidget.printPdf;
+    return sharePdf != oldWidget.sharePdf ||
+        printPdf != oldWidget.printPdf ||
+        loadSubjects != oldWidget.loadSubjects;
   }
 }
