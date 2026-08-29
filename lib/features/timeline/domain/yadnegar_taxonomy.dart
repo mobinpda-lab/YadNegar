c6310380b7dfdceff7628c065c38cf04a61080a9
mobinpda-lab <mobinpda@gmail.com>
2026-08-29 12:55:51 +0330

    feat(taxonomy): add category and tag domain models

---
+++ lib/features/timeline/domain/yadnegar_taxonomy.dart
@@ -0,0 +1,23 @@
+class YadNegarCategory {
+  const YadNegarCategory({
+    required this.id,
+    required this.title,
+    required this.colorValue,
+  });
+
+  final String id;
+  final String title;
+  final int colorValue;
+}
+
+class YadNegarTag {
+  const YadNegarTag({
+    required this.id,
+    required this.title,
+    required this.colorValue,
+  });
+
+  final String id;
+  final String title;
+  final int colorValue;
+}
