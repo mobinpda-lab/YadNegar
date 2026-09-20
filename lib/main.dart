a8cc97f31380a3704bc8d7b5066bc0faaa98c130
mobinpda-lab <mobinpda@gmail.com>
2026-09-20 20:06:58 +0330

    fix(search): remove invalid home taxonomy constructor argument

--- lib/main.dart
+++ lib/main.dart
@@ -253,7 +253,6 @@ Future<void> main() async {
     loadFollowUps: loadFollowUps,
     addFollowUp: addFollowUp,
     editTimelineItem: editTimelineItem,
-    manageTaxonomy: manageTaxonomy,
     reminderScheduler: reminderScheduler,
     legacyTimeline: toolsHub,
   );
